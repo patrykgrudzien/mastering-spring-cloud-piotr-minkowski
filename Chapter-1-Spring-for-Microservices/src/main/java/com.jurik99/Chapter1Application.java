4147c081b756e30d6ba3ce9b1d7eea6e7856d5b1
patrykgrudzien <patryk.grudzien.91@gmail.com>
2019-05-16 11:57:17 +0200

    Deploying applications with Spring Boot

---
+++ Chapter-1-Spring-for-Microservices/src/main/java/com.jurik99/Chapter1Application.java
@@ -0,0 +1,12 @@
+package com.jurik99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter1Application {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(Chapter1Application.class);
+	}
+}
