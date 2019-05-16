91b3911bd8998909a0c05d97b1c9d7a0100ba714
patrykgrudzien <patryk.grudzien.91@gmail.com>
2019-05-16 13:37:38 +0200

    Customizing configuration files

--- Chapter-1-Spring-for-Microservices/src/main/java/com.jurik99/Chapter1Application.java
+++ Chapter-1-Spring-for-Microservices/src/main/java/com.jurik99/Chapter1Application.java
@@ -2,8 +2,10 @@ package com.jurik99;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(Config.class)
 public class Chapter1Application {
 
 	public static void main(final String[] args) {
---
+++ Chapter-1-Spring-for-Microservices/src/main/java/com.jurik99/Config.java
@@ -0,0 +1,15 @@
+package com.jurik99;
+
+import lombok.Getter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ConfigurationProperties(prefix = "my")
+public class Config {
+
+	@Getter
+	private List<String> servers = new ArrayList<String>();
+}
