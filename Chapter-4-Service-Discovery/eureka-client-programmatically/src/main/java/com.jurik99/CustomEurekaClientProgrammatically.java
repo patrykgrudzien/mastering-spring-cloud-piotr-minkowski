b25acc80bc23aff701a4f4fbfd0702036557b7e6
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2019-05-31 16:52:39 +0200

    Using discovery client programmatically

---
+++ Chapter-4-Service-Discovery/eureka-client-programmatically/src/main/java/com.jurik99/CustomEurekaClientProgrammatically.java
@@ -0,0 +1,12 @@
+package com.jurik99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomEurekaClientProgrammatically {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(CustomEurekaClientProgrammatically.class);
+    }
+}
---
+++ Chapter-4-Service-Discovery/eureka-client-programmatically/src/main/java/com.jurik99/resource/ServiceInstanceResource.java
@@ -0,0 +1,30 @@
+package com.jurik99.resource;
+
+import lombok.extern.log4j.Log4j2;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Log4j2
+@RestController
+public class ServiceInstanceResource {
+
+    private final DiscoveryClient discoveryClient;
+
+    public ServiceInstanceResource(final DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @GetMapping("/ping")
+    public List<ServiceInstance> ping() {
+        final List<ServiceInstance> instances = discoveryClient.getInstances("CLIENT-SERVICE");
+        log.info("INSTANCES: count={}", instances.size());
+
+        instances.forEach(instance -> log.info("INSTANCE: id={}, port={}", instance.getServiceId(), instance.getPort()));
+        return instances;
+    }
+}
